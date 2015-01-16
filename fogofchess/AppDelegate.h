2cbe4b19f9245bca06919393290f6e8d50f0f987
Case Commons <ccUser@inwood.nyc.casecommons.org>
2015-01-16 18:28:20 -0500

    Initial Commit

---
+++ fogofchess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fogofchess
+//
+//  Created by Case Commons on 1/16/15.
+//  Copyright (c) 2015 Kaminsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
